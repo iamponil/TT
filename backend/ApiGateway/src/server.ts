1e67a4b2689a4a7341a97a3fdcb0423bf42531b7
iamponil <ameur.nemlaghi@hotmail.fr>
2025-12-10 03:17:09 +0100

    feat: complete tested backend

---
+++ backend/ApiGateway/src/server.ts
@@ -0,0 +1,37 @@
+import express, { Application } from 'express';
+import http from 'http';
+import cors from 'cors';
+import Routes from './routes';
+
+class ServerClass {
+  private app: Application;
+  private httpServer: http.Server;
+
+  constructor() {
+    this.app = express();
+    this.httpServer = http.createServer(this.app);
+
+    this.configureMiddleware();
+  }
+
+  private configureMiddleware() {
+    this.app.use(cors());
+
+    // Parse JSON bodies for all requests
+    this.app.use(express.json());
+  }
+
+  private configureRoutes() {
+    new Routes(this.app).init();
+  }
+
+  public start() {
+    this.configureRoutes();
+    const PORT = process.env.API_GATEWAY_PORT || 8000;
+    this.httpServer.listen(PORT, () =>
+      console.log(`API Gateway running on http://localhost:${PORT}`)
+    );
+  }
+}
+
+export default new ServerClass();
